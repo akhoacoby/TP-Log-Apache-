274951ac5e9d5536658d0d6f4f9509c63d47115c
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-01-22 17:27:08 +0100

    Create struct.h

---
+++ struct.h
@@ -0,0 +1,30 @@
+typedef struct time_zone {
+    int signe;   // +1 ou -1 
+    unsigned int hour; // déclage en valeur absolue
+    unsigned int minute;  
+} TimeZone;
+
+enum class Month {
+    JAN = 1,    
+    FEB = 2,   
+    MAR = 3,  
+    APR = 4,    
+    MAY = 5,    
+    JUN = 6,    
+    JUL = 7,   
+    AUG = 8,    
+    SEP = 9,    
+    OCT = 10,   
+    NOV = 11,  
+    DEC = 12  
+};
+
+typedef struct Date {
+    unsigned int day;  
+    Month month;       
+    int year;           
+    unsigned int hour;  
+    unsigned int minute; 
+    unsigned int second; 
+    TimeZone zone;      
+} Date;
