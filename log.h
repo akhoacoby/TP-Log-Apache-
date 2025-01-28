60c96d9b397eafb708741b47d15aad8b269779da
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-01-28 13:05:00 +0100

    Update log.h
    Correct syntax errors

--- log.h
+++ log.h
@@ -1,54 +1,30 @@
-#IF ! DEFINED (LOG_H)
-#DEFINE LOG_H
+#ifndef LOG_H
+#define LOG_H
 
 #include <iostream>
 #include <string>
 #include "struct.h"
-using namespace :stl
+using namespace std;
 
-class Log: {
-friend class Stats;
-friend class Lecture;
+class Log {
+    friend class Stats;
+    friend class Lecture;
+    friend ostream& operator<<(ostream& os, const Log& l);
+
 public:
-  ostream& operator<<(ostream& os, const Log& l)
-  {
-      os << ;
-      return os;
-  };
-  Log ():
- IP(""), User_Logname(""), Auth_User(""), status(0), size(0), referer(""), id_client("")
-    {
-      TimeZone init_time_zone;
-      Request_info init_request;
-      init_time_zone.signe = 1;
-      init_time_zone.hour = 0;
-      init_time_zone.minute = 0;
-      init_request.action = GET;
-      init_request.url = "";
-      init_request.protocol = "";
-      init_request.extension = "";
-      Date_time.day = 1;
-      Date_time.month = JAN;
-      Date_time.year = 0;
-      Date_time.hour = 0;
-      Date_time.minute = 0;
-      Date_time.second = 0;
-      Date_time.zone = init_time_zone;
-    };
-  virtual ~Log ();
-  Log(const str log):{
+    Log();
+    Log(const string& log);
+    virtual ~Log();
     
-  }
-  
 protected:
-  str IP;
-  str User_Logname;
-  str Auth_User;
-  Date Date_time;
-  Request_info Request;
-  int status;
-  unsigned int size;
-  str referer;
-  str id_client;
+    string IP;
+    string User_Logname;
+    string Auth_User;
+    Date Date_time;          
+    Request_info Request;   
+    int status;
+    unsigned int size;
+    string referer;
+    string id_client;
 };
 #endif
