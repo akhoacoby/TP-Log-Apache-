8a0aa9e75125b1facc261f0347f2f77467bc855d
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-01-28 22:32:10 +0100

    Update struct.h

--- struct.h
+++ struct.h
@@ -2,37 +2,38 @@
 #define STRUCT_H
 
 #include <string>
+#include <iostream>
 using namespace std;
 
 typedef struct time_zone {
-    int signe;   // +1 ou -1 
+    int signe;   // +1 ou -1
     unsigned int hour; // déclage en valeur absolue
-    unsigned int minute;  
+    unsigned int minute;
 } TimeZone;
 
 enum class Month {
-    JAN = 1,    
-    FEB = 2,   
-    MAR = 3,  
-    APR = 4,    
-    MAY = 5,    
-    JUN = 6,    
-    JUL = 7,   
-    AUG = 8,    
-    SEP = 9,    
-    OCT = 10,   
-    NOV = 11,  
-    DEC = 12  
-}; 
+    JAN = 1,
+    FEB = 2,
+    MAR = 3,
+    APR = 4,
+    MAY = 5,
+    JUN = 6,
+    JUL = 7,
+    AUG = 8,
+    SEP = 9,
+    OCT = 10,
+    NOV = 11,
+    DEC = 12
+  };
 
 typedef struct Date {
-    unsigned int day;  
-    Month month;       
-    int year;           
-    unsigned int hour;  
-    unsigned int minute; 
-    unsigned int second; 
-    TimeZone zone;      
+    unsigned int day;
+    Month month;
+    int year;
+    unsigned int hour;
+    unsigned int minute;
+    unsigned int second;
+    TimeZone zone;
 } Date;
 
 enum class Action_type {
@@ -49,5 +50,8 @@ typedef struct Request_info {
     string protocol;
     string extension;
 } Request_info;
+
+ostream& operator<<(std::ostream& os, Month month);
+ostream& operator<<(std::ostream& os, Action_type action);
 
 #endif
