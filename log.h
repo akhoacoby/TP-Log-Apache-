2ce265cf760285451739609e13463f00c5f94783
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-01-28 22:34:26 +0100

    Update log.h
    log l isn't passed by reference in the overload of operator <<

--- log.h
+++ log.h
@@ -3,28 +3,28 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 #include "struct.h"
 using namespace std;
 
 class Log {
-    friend class Stats;
-    friend class Lecture;
-    friend ostream& operator<<(ostream& os, const Log& l);
-
+  friend class Stats;
+  friend class Lecture;
+  friend ostream& operator<<(ostream& os, const Log l);
 public:
-    Log();
-    Log(const string& log);
-    virtual ~Log();
-    
+  Log();
+  Log(const string log);
+  virtual ~Log ();
 protected:
-    string IP;
-    string User_Logname;
-    string Auth_User;
-    Date Date_time;          
-    Request_info Request;   
-    int status;
-    unsigned int size;
-    string referer;
-    string id_client;
+  string IP;
+  string User_Logname;
+  string Auth_User;
+  Date Date_time;
+  Request_info Request;
+  int status;
+  unsigned int size;
+  string referer;
+  string id_client;
 };
 #endif
