054ed7d745d5499ce35e5cb3103d2b58f9ad8ed8
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-01-29 15:42:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include "simplelog.h"
 #include "log.h"
 
 using namespace std;
 
 int main() {
 
-    // Lines from exemple-mini-non-exhaustif.txt that have to be given as string by class Lecture
+    // Lines from exemple-mini-non-exhaustif.txt that have to be given as str by class Lecture
     string test2 = "192.168.0.1 - - [08/Sep/2012:11:15:00 +0200] \"GET /page2.html HTTP/1.1\" 200 2000 \"http://intranet-if.insa-lyon.fr/page1.html\" \"Mozilla/5.0\"";
     string test3 = "192.168.0.1 - - [08/Sep/2012:11:37:00 +0200] \"GET /page1.html HTTP/1.1\" 200 1000 \"http://intranet-if.insa-lyon.fr/page2.html\" \"Mozilla/5.0\"";
     string test4 = "192.168.0.1 - - [08/Sep/2012:12:12:00 +0200] \"GET /page2.html HTTP/1.1\" 200 2000 \"http://intranet-if.insa-lyon.fr/page3.html\" \"Mozilla/5.0\"";
@@ -21,6 +22,7 @@ int main() {
     Log test_log5(test5);
     Log test_log6(test6);
     Log test_log7(test7);
+    SimpleLog test_simple_log(test_log7);
 
     // Print log infos
     cout << test_log2;
@@ -28,11 +30,14 @@ int main() {
     cout << test_log4;
     cout << test_log5;
     cout << test_log6;
-    cout << test_log7;
+    cout << test_simple_log;
 
     // Test default constructor
     Log test_log_default;
     cout << test_log_default;
+
+    SimpleLog test_simple_log_default;
+    cout << test_simple_log_default;
 
     return 0;
 }
