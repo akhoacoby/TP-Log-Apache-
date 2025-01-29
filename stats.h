b64a2725b3e93fb0a92ef35ff05848253998f5cc
Mm-12324 <malakbahda11@gmail.com>
2025-01-29 15:45:09 +0100

    Create stats.h

---
+++ stats.h
@@ -0,0 +1,38 @@
+#ifndef STATS_H
+#define STATS_H
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+using namespace std; 
+
+
+class Stats {
+public:
+    Stats();  
+    // Constructeur par défaut
+
+    void ajouter(const Log& nouveau_log);  
+    // Ajouter un log
+
+    void afficherTop10() const;  
+    // Afficher le top 10 des pages consultées
+
+    void genererGraphViz(const string& fichier_dot) const;  
+    // Générer un fichier GraphViz
+
+    void filtrerExtensions() ;
+    //Filtrer par extension
+
+    void filtrerParHeure(int heure);
+    //Filtrer par heure
+
+private:
+    unordered_map<string, pair<Date_time, string>> url_map;  
+    // La map qui stocke les informations des URLs
+};
+
+#endif
