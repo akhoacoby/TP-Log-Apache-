595f7db55ce19e11afb3d7b95268e38d3f4ef6a9
Nathan Lèbre <73109986+KML-Style@users.noreply.github.com>
2025-01-31 01:12:00 +0100

    Update struct.cpp

--- struct.cpp
+++ struct.cpp
@@ -62,3 +62,84 @@ ostream& operator<<(ostream& os, const Action_type action) {
    }
    return os;
 };
+
+bool Date::isLeapYear(int y){
+  if (y % 400 == 0){
+    return true;
+  }
+  else if (y % 4 == 0 && y % 100 != 0){
+    return true;
+  }
+  else{
+    return false;
+  }
+};
+
+int Date::daysInMonth(Month m, int y) {
+    switch (m) {
+        case Month::JAN: case Month::MAR: case Month::MAY: case Month::JUL:
+        case Month::AUG: case Month::OCT: case Month::DEC:
+            return 31;
+        case Month::APR: case Month::JUN: case Month::SEP: case Month::NOV:
+            return 30;
+        case Month::FEB:
+          if (isLeapYear(y)){
+            return 29;
+          }
+          else{
+            return 28;
+          }
+        default:
+          return 31;
+    }
+};
+
+string Date::to_string(){
+  int time = hour * 3600 + minute * 60 + second;
+  int temp_day = day;
+  Month temp_month = month;
+  int temp_year = year;
+  int jet_lag = zone.signe * (zone.hour * 3600 + zone.minute * 60);
+  int absolute_time = time + jet_lag;
+
+  if (absolute_time >= 24 * 3600) {
+    absolute_time -= 24 * 3600;
+    temp_day++;
+
+
+    if (temp_day > daysInMonth(temp_month, temp_year)) {
+      temp_day = 1;
+      temp_month = static_cast<Month>(static_cast<int>(temp_month) + 1);
+
+      if (temp_month > Month::DEC) {
+          temp_month = Month::JAN;
+          temp_year++;
+      }
+    }
+  }
+  else if (absolute_time < 0) {
+    absolute_time += 24 * 3600;
+    temp_day--;
+
+
+    if (temp_day == 0) {
+      temp_month = static_cast<Month>(static_cast<int>(temp_month) - 1);
+
+      if (temp_month < Month::JAN) {
+          temp_month = Month::DEC;
+          temp_year--;
+      }
+      temp_day = daysInMonth(temp_month, temp_year);
+    }
+  }
+
+
+int new_hour = absolute_time / 3600;
+int new_minute = (absolute_time % 3600) / 60;
+int new_second = absolute_time % 60;
+
+
+return (std::to_string(temp_day) + "/" + std::to_string(static_cast<int>(temp_month)) + "/" +
+            std::to_string(temp_year) + " " + std::to_string(new_hour) + ":" +
+            std::to_string(new_minute) + ":" + std::to_string(new_second));
+};
